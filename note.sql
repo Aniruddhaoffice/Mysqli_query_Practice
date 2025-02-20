d8e6fc3a4bd784263f4e47228db68cea032757e3
Aniruddhaoffice <144651519+Aniruddhaoffice@users.noreply.github.com>
2025-02-21 00:06:08 +0530

    L1

--- note.sql
+++ note.sql
@@ -16,3 +16,14 @@ show tables;
 -- show table structure
 desc tablename;
 desc students;
+
+-- TABLE alteration
+-- add column
+alter table tablename add columnname datatype;
+alter table students add email varchar(100);
+-- modify column
+alter table [tablename] MODIFY [columnname] [newdatatype];
+alter table students MODIFY phone varchar(15) not null;
+-- rename column
+ALTER TABLE [tablename] CHANGE COLUMN [old_name] [new_column_name][datatype];
+ ALTER TABLE students CHANGE COLUMN phone mobile VARCHAR(20);
